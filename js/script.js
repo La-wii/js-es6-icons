61dda505c2f010bc98f0538cd7c363531708f65b
La-wii <sentilavale@gmail.com>
2021-06-18 19:31:43 +0200

    new init

--- js/script.js
+++ js/script.js
@@ -1,125 +1,18 @@
 const icons = [
-    {
-      name: 'cat',
-      prefix: 'fa-',
-      type: 'animal',
-      family: 'fas',
-    },
-    {
-      name: 'crow',
-      prefix: 'fa-',
-      type: 'animal',
-      family: 'fas',
-    },
-    {
-      name: 'dog',
-      prefix: 'fa-',
-      type: 'animal',
-      family: 'fas',
-    },
-    {
-      name: 'dove',
-      prefix: 'fa-',
-      type: 'animal',
-      family: 'fas',
-    },
-    {
-      name: 'dragon',
-      prefix: 'fa-',
-      type: 'animal',
-      family: 'fas',
-    },
-    {
-      name: 'horse',
-      prefix: 'fa-',
-      type: 'animal',
-      family: 'fas',
-    },
-    {
-      name: 'hippo',
-      prefix: 'fa-',
-      type: 'animal',
-      family: 'fas',
-    },
-    {
-      name: 'fish',
-      prefix: 'fa-',
-      type: 'animal',
-      family: 'fas',
-    },
-    {
-      name: 'carrot',
-      prefix: 'fa-',
-      type: 'vegetable',
-      family: 'fas',
-    },
-    {
-      name: 'apple-alt',
-      prefix: 'fa-',
-      type: 'vegetable',
-      family: 'fas',
-    },
-    {
-      name: 'lemon',
-      prefix: 'fa-',
-      type: 'vegetable',
-      family: 'fas',
-    },
-    {
-      name: 'pepper-hot',
-      prefix: 'fa-',
-      type: 'vegetable',
-      family: 'fas',
-    },
-    {
-        name: 'user-astronaut',
-      prefix: 'fa-',
-      type: 'user',
-      family: 'fas',
-    },
-    {
-      name: 'user-graduate',
-      prefix: 'fa-',
-      type: 'user',
-      family: 'fas',
-    },
-    {
-      name: 'user-ninja',
-      prefix: 'fa-',
-      type: 'user',
-      family: 'fas',
-    },
-    {
-      name: 'user-secret',
-      prefix: 'fa-',
-      type: 'user',
-      family: 'fas',
-    },
+    {name: 'cat', prefix: 'fa-', type: 'animal', family: 'fas',},
+    {name: 'crow', prefix: 'fa-', type: 'animal', family: 'fas',},
+    {name: 'dog', prefix: 'fa-', type: 'animal', family: 'fas',},
+    {name: 'dove', prefix: 'fa-', type: 'animal',family: 'fas',},
+    {name: 'dragon', prefix: 'fa-', type: 'animal',family: 'fas',},
+    {name: 'horse', prefix: 'fa-', type: 'animal', family: 'fas',},
+    {name: 'hippo', prefix: 'fa-', type: 'animal', family: 'fas',},
+    {name: 'fish', prefix: 'fa-', type: 'animal', family: 'fas',},
+    {name: 'carrot', prefix: 'fa-', type: 'vegetable',family: 'fas',},
+    {name: 'apple-alt',prefix: 'fa-', type: 'vegetable', family: 'fas',},
+    {name: 'lemon', prefix: 'fa-', type: 'vegetable',family: 'fas',},
+    {name: 'pepper-hot', prefix: 'fa-', type: 'vegetable', family: 'fas',},
+    {name: 'user-astronaut', prefix: 'fa-', type: 'user', family: 'fas',},
+    {name: 'user-graduate',prefix: 'fa-', type: 'user',family: 'fas',},
+    {name: 'user-ninja', prefix: 'fa-', type: 'user', family: 'fas',},
+    {name: 'user-secret', prefix: 'fa-', type: 'user',family: 'fas',},
 ];
-
-console.log(icons);
-
-icons.forEach((element) => {
-    const {name,prefix,family} = element;
-
-    const elementHTML = `
-    <div class="col-2 border border-secondary altezza d-flex align-items-center justify-content-center me-3" id="stampa">
-        <i class="${family} ${prefix} ${name}"></i>
-    </div> 
-   
-    `;
-    document.getElementById("stampa").innerHTML += elementHTML;
-
-});
-
-
-
-
-
-
-
-
-
-
-
-
